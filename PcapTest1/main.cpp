e3fb41fe66eb4afd99c046699f8d0f08ad6af327
root <root@localhost.localdomain>
2018-04-08 12:52:19 -0400

    IP패킷 정보 확인

--- PcapTest1/main.cpp
+++ PcapTest1/main.cpp
@@ -1,74 +1,15 @@
-#include <sys/time.h>
-#include <netinet/in.h>
-#include <net/ethernet.h>
-#include <pcap/pcap.h>
-#include <signal.h>
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
+#include <pcap.h>
+#include <sys/socket.h>
+#include <netinet/in_systm.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <netinet/ip_icmp.h>
+#include <net/if.h>
+#include <netinet/if_ether.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
-
-
-struct ip *iph;
-struct tcphdr *tcph;
-
-void callback(u_char *useless, const struct pcap_pkthdr *pkthdr,
-                const u_char *packet)
-{
-    static int count = 1;
-    struct ether_header *ep;
-    unsigned short ether_type;
-    int chcnt =0;
-    int length=pkthdr->len;
-
-    // 이더넷 헤더를 가져온다.
-    ep = (struct ether_header *)packet;
-
-    // IP 헤더를 가져오기 위해서
-    // 이더넷 헤더 크기만큼 offset 한다.
-    packet += sizeof(struct ether_header);
-
-    // 프로토콜 타입을 알아낸다.
-    ether_type = ntohs(ep->ether_type);
-
-    // 만약 IP 패킷이라면
-    if (ether_type == ETHERTYPE_IP)
-    {
-        // IP 헤더에서 데이타 정보를 출력한다.
-        iph = (struct ip *)packet;
-        printf("IP 패킷\n");
-        printf("Version     : %d\n", iph->ip_v);
-        printf("Header Len  : %d\n", iph->ip_hl);
-        printf("Ident       : %d\n", ntohs(iph->ip_id));
-        printf("TTL         : %d\n", iph->ip_ttl);
-        printf("Src Address : %s\n", inet_ntoa(iph->ip_src));
-        printf("Dst Address : %s\n", inet_ntoa(iph->ip_dst));
-
-
-    printf("\n\n");
-    }
-}
-int main(int argc, char **argv)
-{
-    char *dev;    // 네트워크 디바이스
-    char *net;    // 네트워크 어드레스
-    char *mask;   // 네트워크 mask 어드레스
-    int ret;      //
-    char errbuf[PCAP_ERRBUF_SIZE]; // 에러 발생 시 에러 메시지 저장하는 버퍼
-    bpf_u_int32 netp;  // 네트워크 디바이스의 네트워크 주소가 저장될 주소
-    bpf_u_int32 maskp; // 네트워크 디바이스의 넷마스크 주소가 저장될 주소
-    struct in_addr addr;
-    struct in_addr net_addr, mask_addr;
-    const u_char *packet;
-    pcap_t *pcd;
-
 
     // pcap_t *pcap_open_live(char *device, int snaplen, int promisc, int to_ms, char *ebuf)
     // 현재 네트워크에서 사용하는 네트워크 디바이스의 패킷을 캡처하는 함수
@@ -87,16 +28,28 @@ int main(int argc, char **argv)
     // 패킷의 pcap_pkthdr 구조체를 가리 키도록 설정되고
     // pkt_data 인수가 가리키는 포인터는 패킷의 데이터를 가리 키도록 설정
 
+int main(int argc, char **argv)
+{
+    char *dev;    // 네트워크 디바이스
+    char *net;    // 네트워크 어드레스
+    char *mask;   // 네트워크 mask 어드레스
+    int ret;      //
+    char errbuf[PCAP_ERRBUF_SIZE]; // 에러 발생 시 에러 메시지 저장하는 버퍼
+    bpf_u_int32 netp;  // 네트워크 디바이스의 네트워크 주소가 저장될 주소
+    bpf_u_int32 maskp; // 네트워크 디바이스의 넷마스크 주소가 저장될 주소
+    struct in_addr addr;
+    struct in_addr net_addr, mask_addr;
+    struct pcap_pkthdr *pkt_hdr;
+    const u_char *pkt_data;
+    pcap_t *pcd;
+    int res;
 
+    dev = argv[2];
 
-    dev = argv[2]; // 패킷을 캡쳐할 적당한 네트워크 디바이스 이름을 얻어온다
-
-
-    // 에러가 발생했을경우
     if(dev == NULL)
     {
-        printf("%s\n",errbuf);
-        exit(1);
+        printf("%s\n", errbuf);
+        exit(0);
     }
 
     // 네트워크 디바이스 이름 출력
@@ -109,7 +62,7 @@ int main(int argc, char **argv)
     if(ret == -1) // 에러 발생시
     {
         printf("%s\n",errbuf);
-        exit(1);
+        exit(0);
     }
 
 
@@ -120,7 +73,7 @@ int main(int argc, char **argv)
     if(net == NULL)
     {
         perror("inet_ntoa");
-        exit(1);
+        exit(0);
     }
 
     printf("NET: %s\n",net);
@@ -133,19 +86,65 @@ int main(int argc, char **argv)
     if(mask == NULL)
     {
         perror("inet_ntoa");
-        exit(1);
+        exit(0);
     }
     printf("MASK: %s\n",mask);
     printf("=======================\n");
 
+
     pcd = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-        if (pcd == NULL)
-        {
-            printf("%s\n", errbuf);
-            exit(1);
-        }
+    if (pcd == NULL)
+    {
+        printf("%s\n", errbuf);
+        exit(0);
+    }
 
+/*        ret1 = pcap_next_ex(pcd, &pkt_hdr, &pkt_data);
+    if (ret1 == NULL)
+    {
+       printf("No packet found.\n");
+       exit(1);
+    }*/
 
+    struct ip *iph;
+    struct tcphdr *tcph;
 
-    pcap_loop(pcd, atoi(argv[1]), callback, NULL);
+    struct ether_header *ep;
+    unsigned short ether_type;
+
+    while((res = pcap_next_ex(pcd, &pkt_hdr, &pkt_data))>=0)
+    {
+        if(res==0) continue;
+
+    ep = (struct ether_header *)pkt_data;
+
+    // IP 헤더를 가져오기 위해서
+    // 이더넷 헤더 크기만큼 offset 한다.
+    pkt_data += sizeof(struct ether_header);
+
+    // 프로토콜 타입을 알아낸다.
+    ether_type = ntohs(ep->ether_type);
+
+    // 만약 IP 패킷이라면
+    if (ether_type == ETHERTYPE_IP)  // DST + SRC + EtherType (0x080 = ip)
+    {
+        // IP 헤더에서 데이타 정보를 출력한다.
+        iph = (struct ip *)pkt_data;
+        printf("IP 패킷\n");
+        printf("Version     : %d\n", iph->ip_v);
+        printf("Header Len  : %d\n", iph->ip_hl);
+        printf("TTL         : %d\n", iph->ip_ttl); // TTL = IP 패킷이 라우터를 지나칠 때마다 라우터 TTL값 -1
+        printf("SRC IP      : %s\n", inet_ntoa(iph->ip_src));
+        printf("DST IP      : %s\n", inet_ntoa(iph->ip_dst));
+        printf("\n");
+    }
+    // -1 == 에러가 발생했을 때
+    if (res == -1)
+    {
+       printf("Error : %s\n",errbuf);
+       break;
+    }
+  }
 }
+
+
